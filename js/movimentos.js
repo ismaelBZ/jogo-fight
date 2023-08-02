c62186b13b05f3f005ddad2c1f1442533d22c8b0
Ismael <ismaelbetzoletti@gmail.com>
2023-08-02 18:26:44 -0300

    começando a implementar física

--- js/movimentos.js
+++ js/movimentos.js
@@ -1,18 +1,41 @@
-// VARIÁVEIS DO HTML
+/*** Física ***/
+
+// Posições
+let posicaoInicial = 0;
+let posicaoFinal;
+let altura;
+let posicaoDoPersonagem = posicaoInicial;
+
+//Tempo
+let tempoDeDeslocamento;
+let tempoDeAceleracao;
+
+// Movimentação
+let velocidade = 10;
+let aceleracao = 1;
+let aceleraçãoDaGravidade = 9.8;
+
+
 const personagemEsquerda = document.getElementById("personagem-esquerda");
-let posicaoPersonagemEsquerda = 0;
+posicaoInicial = 0;
+// let posicaoPersonagemEsquerdaVerticalInicial = 35;
+// let alturaPersonagemEsquerda = 300;
 
 // TECLAS DE MOVIMENTAÇÃO
-const setaDireita = 39;
-const setaEsquerda = 37;
+const setaParaDireita = 39;
+const setaParaEsquerda = 37;
+const setaParaCima = 38;
+const setaParaBaixo = 38;
 
-function movimentarPersonagemEsquerda(event) {
-  if (event.keyCode == setaDireita) {
-    posicaoPersonagemEsquerda += 50;
-  } else if ((event.keyCode = setaEsquerda)) {
-    posicaoPersonagemEsquerda -= 50;
-  }
-  personagemEsquerda.style.left = `${posicaoPersonagemEsquerda}px`;
+function movimentarPersonagem(event) {
+  console.log(event);
+  // setInterval((aceleracao) => aceleracao += 1, 300)
+  // if (event.keyCode == setaParaDireita) {
+  //   posicaoDoPersonagem += velocidade * aceleracao;
+  // } else if ((event.keyCode = setaParaEsquerda)) {
+  //   posicaoDoPersonagem -= velocidade * aceleracao;
+  // }
+  // personagemEsquerda.style.left = `${posicaoDoPersonagem}px`;
 }
 
-document.onkeydown = movimentarPersonagemEsquerda;
+document.keypress = movimentarPersonagem;
