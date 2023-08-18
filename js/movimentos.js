3bbc518f79eb8c51a63398787193f57fff45782c
Ismael <ismaelbetzoletti@gmail.com>
2023-08-18 17:51:37 -0300

    fixado deslocamento máximo do personagem para o tamanho da tela

--- js/movimentos.js
+++ js/movimentos.js
@@ -4,7 +4,6 @@
 let posicaoInicial = 0;
 let posicaoFinal;
 let altura;
-let posicaoDoPersonagem = posicaoInicial;
 
 //Tempo
 let tempoDeDeslocamento;
@@ -15,42 +14,47 @@ let velocidade = 10;
 let aceleracao = 2;
 let aceleraçãoDaGravidade = 9.8;
 
-// Funcionalidade
-let stop = true
-
-
-const personagemEsquerda = document.getElementById("personagem-esquerda");
-posicaoInicial = 0;
-// let posicaoPersonagemEsquerdaVerticalInicial = 35;
-// let alturaPersonagemEsquerda = 300;
-
-// TECLAS DE MOVIMENTAÇÃO
+// Teclas de Movimentação
 const setaParaDireita = 39;
 const setaParaEsquerda = 37;
 const setaParaCima = 38;
-const setaParaBaixo = 38;
+const setaParaBaixo = 40;
+
+//Cenario
+const cenario = document.querySelector(".tamanho-de-tela");
+const larguraDoCenarioEmPx = window.getComputedStyle(cenario).getPropertyValue('width');
+const larguraDoCenario = parseInt((larguraDoCenarioEmPx.slice(0, larguraDoCenarioEmPx.length - 2)));
+
+//Personagem
+const personagemEsquerda = document.getElementById("personagem-esquerda");
+const larguraDoPersonagemEmPx = window.getComputedStyle(personagemEsquerda).getPropertyValue("width");
+const larguraDoPersonagem = parseInt(larguraDoPersonagemEmPx.slice(0, larguraDoPersonagemEmPx.length - 2));
+let posicaoDoPersonagem = posicaoInicial;
+// let alturaPersonagemEsquerda = 300;
+
+// Funcionalidade
+let stop = true
+
 
 function movimentarPersonagem(event) {
 
   velocidade += aceleracao;
 
-  if (event.keyCode == setaParaDireita) {
+  if (event.keyCode == setaParaDireita && posicaoDoPersonagem <= (larguraDoCenario - (1.15 * larguraDoPersonagem))) {
     posicaoDoPersonagem += velocidade;
-  } else if ((event.keyCode = setaParaEsquerda)) {
+  } else if ((event.keyCode == setaParaEsquerda && posicaoDoPersonagem >= (larguraDoPersonagem * 0.15))) {
     posicaoDoPersonagem -= velocidade;
   }
 
   personagemEsquerda.style.left = `${posicaoDoPersonagem}px`;
-  
-  console.log(velocidade);
-  
+  console.log(posicaoDoPersonagem)
+  // console.log(posicaoDoPersonagem + larguraDoPersonagem)
 }
 
 function pararPersonagem() {
 
   velocidade = 10;
-  aceleracao = 1;
-  console.log(velocidade);
+  aceleracao = 2;
 
 };
   
@@ -58,7 +62,3 @@ function pararPersonagem() {
 document.onkeydown = movimentarPersonagem;
 document.onkeyup = pararPersonagem;
 
-/** document.onkeyUp colocar uma variavel aqui para parar de executar a função setInterval
- * por exemplo let stop = true -- onkeydown stop = true if (stop) execute setINterval else break
- * onkeyup stop = false
- **/
